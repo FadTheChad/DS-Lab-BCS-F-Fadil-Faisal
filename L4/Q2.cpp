f43b7505d79582af9d3f014aea454386cd756df1
Fad F <90547391+FadTheChad@users.noreply.github.com>
2025-09-18 23:31:10 +0500

    Update Q2.cpp

--- L4/Q2.cpp
+++ L4/Q2.cpp
@@ -1,75 +1,42 @@
 #include <iostream>
-
 using namespace std;
 
-int main() 
+int main()
 {
     int n;
-    cout << "Enter number of employees: ";
     cin >> n;
-
-    int employees[100];
-    cout << "Enter " << n << " employee IDs (sorted):\n";
-
-    for (int i = 0; i < n; i++) 
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        cin >> employees[i];
+        cin >> arr[i];
     }
 
-    int regNo;
-    cout << "Enter your roll number: ";
-    cin >> regNo;
-
-    int suffix = regNo % 100;
-
-    int low = 0, high = n - 1, foundPos = -1;
-    while (low <= high) 
+    cout << "Unsorted Array: ";
+    for (int i = 0; i < n; i++)
     {
-        int mid = (low + high) / 2;
-
-        if (employees[mid] % 100 == suffix) 
-        {
-            foundPos = mid;
-            break;
-        }
-        else if (employees[mid] % 100 < suffix) 
-        {
-            low = mid + 1;
-        }
-        else 
-        {
-            high = mid - 1;
-        }
+        cout << arr[i] << " ";
     }
-
-    if (foundPos != -1) 
-    {
-        cout << "Match found: Employee ID " << employees[foundPos] 
-             << " at index " << foundPos << endl;
-    } 
-    else 
-    {
-        foundPos = low;
-        int newID = (employees[0] / 100) * 100 + suffix;
-
-        for (int j = n; j > foundPos; j--) 
-        {
-            employees[j] = employees[j - 1];
-        }
-
-        employees[foundPos] = newID;
-        n++;
-
-        cout << "No match found. Inserted new Employee ID " << newID 
-             << " at index " << foundPos << endl;
-    }
-
-    cout << "Updated list: ";
-    for (int i = 0; i < n; i++) 
-    {
-        cout << employees[i] << " ";
-    }
-
     cout << endl;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
