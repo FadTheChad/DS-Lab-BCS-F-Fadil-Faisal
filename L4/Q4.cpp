4a0fbd1ae2672b801ecb0f0a2bd96c02ac669664
Fad F <90547391+FadTheChad@users.noreply.github.com>
2025-09-18 23:32:19 +0500

    Update Q4.cpp

--- L4/Q4.cpp
+++ L4/Q4.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
-
 using namespace std;
 
-int main() 
+int main()
 {
-    int arr[10] = {5, 1, 3, 6, 2, 9, 7, 4, 8, 10};
-
-    int n = 10;
-    for (int i = 0; i < n - 1; i++) 
+    int n, target;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++) 
-        {
-            if (arr[j] < arr[j + 1]) 
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+        cin >> arr[i];
     }
 
-    cout << "Sorted array (descending): ";
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 
-    for (int i = 0; i < n; i++) 
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-
     cout << endl;
+
+    cin >> target;
+    int left = 0, right = n - 1;
+    bool found = false;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+        {
+            cout << "Index: " << mid << endl;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Element not found" << endl;
+    }
 
     return 0;
 }
